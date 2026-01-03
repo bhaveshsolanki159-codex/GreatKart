965394e7a1dec4e4e8fb2d3794e23361a2f20f9b
bhaveshsolanki159-codex <bhaveshsolankibj159@gmail.com>
2026-01-03 22:53:52 +0530

    Creat Custom User Model

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'GreatKart/static',
 ]
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
